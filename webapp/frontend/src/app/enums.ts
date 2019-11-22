f2f0cbbaa4b02eb8e0fc5823a597c420f5014c14
Shashwat Black <shashwat.black@gmail.com>
2019-11-22 14:54:48 -0600

    Implemented framework for wizard-style execution.

---
+++ webapp/frontend/src/app/enums.ts
@@ -0,0 +1,11 @@
+export enum DisasterTaxonomies {
+    Earthquake,
+    Hurricane
+}
+
+export enum WizardSteps {
+    DisasterChoice,
+    InputParameters,
+    Simulation,
+    Results,
+}
--- webapp/frontend/src/app/sidebar/sidebar.component.ts
+++ webapp/frontend/src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {DisasterService} from '../disaster.service';
+import {DisasterTaxonomies, WizardSteps} from '../enums';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,16 +9,34 @@ import {DisasterService} from '../disaster.service';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  WizardSteps = WizardSteps;
+  DisasterTaxonomies = DisasterTaxonomies;
+
+  private state: {
+    wizardStep: WizardSteps,
+    chosenDisaster?: DisasterTaxonomies,
+  } = {
+    wizardStep: WizardSteps.DisasterChoice,
+  };
 
   private magnitudeResponse: any;
   private magnitude: string;
   private earthquakeParameters: object;
 
-  constructor(private http: HttpClient, private disasterService: DisasterService) {
-    this.earthquakeParameters = disasterService.earthquakeParameters;
+  chooseDisaster(chosenDisaster: DisasterTaxonomies) {
+    this.state.chosenDisaster = chosenDisaster;
+
+    if (this.state.chosenDisaster === DisasterTaxonomies.Earthquake) {
+      this.disasterService.startEarthquake();
+    } else if (this.state.chosenDisaster === DisasterTaxonomies.Hurricane) {
+      console.log('Method not implemented.');
+    }
+
+    this.state.wizardStep = WizardSteps.InputParameters;
   }
 
-  ngOnInit() {
+  goBack() {
+    this.state.wizardStep = Math.max(0, this.state.wizardStep - 1);
   }
 
   fetchMagnitude(event: Event) {
@@ -31,5 +50,12 @@ export class SidebarComponent implements OnInit {
         },
         err => console.error(err)
       );
+  }
+
+  constructor(private http: HttpClient, private disasterService: DisasterService) {
+    this.earthquakeParameters = disasterService.earthquakeParameters;
+  }
+
+  ngOnInit() {
   }
 }
