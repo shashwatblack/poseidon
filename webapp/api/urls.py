ba71896317d8845a8c8fa7718a400c9450c0a2a9
Shashwat Black <shashwat.black@gmail.com>
2019-11-30 16:15:29 -0600

    Added disaster simulation view.

--- webapp/api/urls.py
+++ webapp/api/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from api import views as api_views
 
 urlpatterns = [
-    url(r'^map/', api_views.MapView.as_view()),
+    url(r'^simulation/', api_views.DisasterSimulationView.as_view()),
     url(r'^earthquake/', api_views.EarthquakeView.as_view()),
 ]
