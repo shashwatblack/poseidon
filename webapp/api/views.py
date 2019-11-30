7b39ae03f26c7410ae78b016242d0069e41bf940
Shashwat Black <shashwat.black@gmail.com>
2019-11-30 11:32:47 -0600

    Added view to plot settlement view.

--- webapp/api/views.py
+++ webapp/api/views.py
@@ -1,8 +1,12 @@
 import json
+import random
+
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
+
+from poseidon.infrastructure.road_network_revised import RoadNetwork
 from poseidon.utils.constants import Constants
 from poseidon.disasters.earthquakes import GaussianEarthquake
 
@@ -38,7 +42,7 @@ class MapView(View):
     def get(self, request):
         requested_map_type = request.GET.get('map_type', Constants.SETTLEMENT_VIEW)
         if requested_map_type == Constants.SETTLEMENT_VIEW:
-            return JsonResponse(self.get_dummy_settlement_view())
+            return JsonResponse(self.get_settlement_view())
         elif requested_map_type == Constants.TILE_VIEW:
             return JsonResponse({})  # todo
 
@@ -64,6 +68,37 @@ class MapView(View):
             edges.append({
                 "start": [float(city1['lat']), float(city1['lng'])],
                 "end": [float(city2['lat']), float(city2['lng'])],
+                "damage": random.randint(0, 100)
+            })
+
+        return {
+            "cities": cities,
+            "edges": edges
+        }
+
+    @staticmethod
+    def get_settlement_view():
+        road_network = RoadNetwork()
+        graph_settlement_view = road_network.graph_settlement_view
+        cities = list()
+        nodes = graph_settlement_view.nodes(data=True)
+        for node, attr in nodes:
+            pos = attr['pos']
+            cities.append({
+                "city": attr['name'],
+                "population": int(attr['population']),
+                "lat": pos.deg_lat,
+                "lng": pos.deg_lon,
+                "damage": random.randint(0, 100)
+            })
+
+        edges = list()
+        for u, v in graph_settlement_view.edges():
+            start = nodes[u]['pos']
+            end = nodes[v]['pos']
+            edges.append({
+                "start": [start.deg_lat, start.deg_lon],
+                "end": [end.deg_lat, end.deg_lon],
                 "damage": random.randint(0, 100)
             })
 
